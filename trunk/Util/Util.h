f0b18a272142c41a0d55f9660ea507abc13bbaf9
vero13 <vero13@f9ba8af8-c0ef-11de-91a4-794fdb80bb57>
2009-11-13 19:11:27 +0000

    esto de la libreria me canso :p

--- trunk/Util/Util.h
+++ trunk/Util/Util.h
@@ -3,17 +3,15 @@
 
 #include <sstream>
 #include <string>
+#include <stdint.h>
 
 /** 
  * Clase con funciones comunes para el programa.
  */
-/****************************************************************************/
 class Util {
 	
-	public:
-
-		static std::string intToString(uint32_t numero);
+public:
+  static std::string intToString(uint32_t numero);
 };
 
-/****************************************************************************/
 #endif /*UTIL_H_*/
