c04881d642c7de937687c94c1620bab54201fff0
gygounet@gmail.com <gygounet@gmail.com@f9ba8af8-c0ef-11de-91a4-794fdb80bb57>
2009-11-17 15:54:49 +0000

    Mas sobre persistencia, no esta terminada, es para mudarme de compu:P tenemos que definir el tema de conexiones y agregar los getter's y setter's al modelo, con esas 2 cosas esta terminado el que guarda casi.

--- XML/Persistidor.h
+++ XML/Persistidor.h
@@ -5,12 +5,13 @@
 #include <fstream>
 #include <string>
 #include "../Componente/Componente.h"
+#include "../XML/Xml.h"
 
 class Persistidor
 {
 	private:
 		std::string nombre;
-		std::string ruta;
+		//std::string ruta;
 		std::string extension;
 		std::fstream archivo;
 	public:
@@ -20,24 +21,51 @@ class Persistidor
 			//this->ruta = "";
 			this->extension = ".bk";
 			//Creo el archivo donde voy a guardar los datos del circuito
-			this->archivo.open(ruta+nombre+extension, std::fstream out);
+			this->archivo.open(/*ruta+*/nombre+extension, std::fstream out);
 			if(!archivo.good()) std::cerr << "Se produjo un error al crear el archivo: " << nombre+extension << std::endl;
 			this->archivo.close();
 		};
 		
-		void persistir(Componente* componente){
-			if(componente){
-				archivo.open(ruta+nombre+extension,std::fstream::in | std::fstream::out);
+		void persistir(Circuito* circuito){
+			if(circuito){
+				archivo.open(/*ruta+*/nombre+extension,std::fstream::in | std::fstream::out);
 				if(archivo.good()){
 					//TODO
-					//Guardar los componenetes en el archivo	
+					//Guardar los componenetes en el archivo
+					
+					//Creo el archivo, establezco la raiz con sus propiedades
+					Xml xml(circuito->nombre.c_str());
+					xml.getRaiz()->setPropiedad("entradas",circuito->getCantidadEntradas());
+					xml.getRaiz()->setPropiedad("salidas",circuito->getCantidadSalidas());
+					//Serializo los componentes del circuito
+					//TODO ver que pasa si el componente es un circuito y no
+					//una compuerta
+					for(int i = 0; i < circuito->getComponentes().size(); i++){
+						Componente* comp = circuito->getComponentes()[i];
+						XmlNodo componente(comp, "");
+						componente.setPropiedad("x",comp->getX());
+						componente.setPropiedad("y",comp->getY());
+						componente.setPropiedad("alfa", comp->getAlfa());
+						componente.setPropiedad("label",comp->getLabel());
+						componente.setPropiedad("tr",comp->getTiempoRetardo());
+						xml.getRaiz()->agregarHijo(componente);
+					}
+					
+					//TODO ver tema de las lineas que conforman la conexion
+					//Serializo las conexiones
+					for(int i = 0; i < circuito->getConexiones().size(); i++){
+						Conexion* conex = circuito->getConexiones()[i];
+						XmlNodo conexion(conex,"");
+						
+					} 
+						
 				}else std::cerr << "No se pudo abrir el archivo: " << nombre+extension << std::endl;
 				archivo.close();
 			}
 		};
 		
-		Componente* recuperar(std::string nombreArchivo){
-			archivo.open(ruta+nombreArchivo+extensio, std::fstream::in);
+		Circuito* recuperar(std::string nombreArchivo){
+			archivo.open(/*ruta+*/nombreArchivo+extensio, std::fstream::in);
 			if(archivo.good()){
 				//TODO
 				//Leer del archivo y crear el componente con todos
