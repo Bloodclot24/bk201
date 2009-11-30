fe3b74f9e69aa1218201ee96ce497c52dc8a5345
gygounet@gmail.com <gygounet@gmail.com@f9ba8af8-c0ef-11de-91a4-794fdb80bb57>
2009-11-30 06:57:42 +0000

    Documentation part II of 201

--- Vista/Dibujos/Compuerta.h
+++ Vista/Dibujos/Compuerta.h
@@ -6,6 +6,11 @@
 #define DIMENSION_COMPUERTA 40
 #define UNIDAD_TIEMPO " ns"
 
+/**
+ * Clase compuerta, es una compuerta generica, de esta clase heredan
+ * todas las compuertas que pueden formar parte del circuito.
+ *
+ */
 class Compuerta: public Dibujo {
 
   std::string   tiempoT;
@@ -25,9 +30,27 @@ public:
 
   virtual ~Compuerta() { };
 
+  /**
+    * Setea el valor del vertice superior izquierdo del circuito, el mismo
+    * se utilizara luego para poder dibujarlo, entre otras cosas.
+    *
+    * @param vSupIzq El nuevo vertice superior izquierdo del circuito.
+    *
+    */
   virtual void setVerticeSupIzq(Vertice vSupIzq);
 
+  /**
+   * Obtiene el tiempo de transicion de la compuerta.
+   *
+   */
   std::string getTiempoT();
+
+  /**
+   * Setea el tiempo de transicion de la compuerta.
+   *
+   * @param tiempoT El nuevo tiempo de transicion de la compuerta.
+   *
+   */
   void setTiempoT(const std::string& tiempoT);
 
   /**
@@ -37,9 +60,26 @@ public:
     */
   virtual void dibujar(const Cairo::RefPtr<Cairo::Context>& context) { };
 
+  /**
+    * Obtiene la cantidad de entradas.
+    *
+    */
   virtual int getCantidadEntradas(){ return 2; }
+
+  /**
+    * Obtiene la cantidad de salidas.
+    *
+    */
   virtual int getCantidadSalidas(){ return 1; }
 
+
+  /**
+     * Dibuja el circuito en el contexto que recibe por parametro, este metodo se
+     * utiliza para imprimir el circuito.
+     *
+     * @param context El contexto sobre el cual se va a dibujar el circuito.
+     *
+     */
   virtual void dibujarImpresion(const Cairo::RefPtr<Cairo::Context>& context) { };
 
 };
